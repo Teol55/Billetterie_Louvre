9fceff2a8631cf574a3aba141f598564ec096894
C_MARTIN <m.ch55000@gmail.com>
2019-02-08 16:42:29 +0100

    modify test after change fixtures

---
+++ tests/src/Entity/VisitorTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tof
+ * Date: 04/02/2019
+ * Time: 15:13
+ */
+
+namespace App\Tests\src\Entity;
+
+
+use App\Entity\Ticket;
+use App\Entity\Visitor;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+
+
+class VisitorTest extends WebTestCase
+{
+
+    /**
+     * @dataProvider dateAgeForVisitor
+     */
+    public function testAge($date,$age)
+    {
+        $order = new Ticket();
+        $order->setDateVisit(new \DateTime('2020-01-01'));
+        $visitor=  new Visitor();
+        $visitor->setOrder($order);
+        $visitor->setBirthday(new \DateTime($date));
+
+
+       $this->assertSame($age,$visitor->age());
+
+
+
+    }
+
+    public function dateAgeForVisitor()
+    {
+        return[['1979-01-15',40],
+                ['2009-04-16',10],
+                ['1978-08-01',41],
+             ['2011-08-07',8],
+            ['1955-02-01',64],
+            ['2018-01-01',2]
+
+        ];
+    }
+
+}
