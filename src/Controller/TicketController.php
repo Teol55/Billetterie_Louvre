aa9b7f38769ff3840504b3e4a0d224f13569fe10
C_MARTIN <m.ch55000@gmail.com>
2019-02-08 16:44:15 +0100

    add function for count visitors by date

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ticket;
+use App\Form\OrderFormType;
+use App\Repository\TicketRepository;
+use App\Repository\VisitorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function index(Request $request, VisitorRepository $Repository)
+    {
+        $form=$this->createForm(OrderFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+           $data = $form->getData();
+//         dd($data['dateVisit']);
+//            $ticket=new Ticket();
+//            $ticket->setDateVisit($data['dateVisit']);
+//            $ticket->setTypeTicket($data['typeTicket']);
+////           $dateVisit=$data['typeTicket'];
+//            dd($ticket);
+
+        dd($Repository->countByDateVisit($data['dateVisit']));
+
+        }
+
+        return $this->render('homepage.html.twig',[
+            'orderForm' => $form->createView()
+
+        ]);
+    }
+    /**
+     * @Route("/test", name="app_test")
+     */
+    public function test(TicketRepository $repository)
+
+    {
+        $orders=$repository->findAll();
+        dd($orders);
+        return $this->render('test.html.twig', [
+
+
+        ]);
+    }
+}
