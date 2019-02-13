4017e22650e2aff0b3221f591acdf99771d2938b
C_MARTIN <m.ch55000@gmail.com>
2019-02-13 21:58:38 +0100

    add function payment and customer

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -3,19 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Ticket;
+use App\Form\CustomerFormType;
 use App\Form\OrderFormType;
+use App\Form\VisitorFormType;
 use App\Repository\TicketRepository;
 use App\Repository\VisitorRepository;
+use App\Service\CalculatePriceVisitor;
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class TicketController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(Request $request, VisitorRepository $Repository)
+    public function index(Request $request, VisitorRepository $Repository,SessionInterface $session)
     {
         $form=$this->createForm(OrderFormType::class);
 
@@ -23,19 +29,14 @@ class TicketController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-           $data = $form->getData();
-//           dd($data);
-//            $ticket=new Ticket();
-//            $ticket->setDateVisit($data['dateVisit']);
-//         dd($data['dateVisit']);
-//            $ticket=new Ticket();
-//            $ticket->setDateVisit($data['dateVisit']);
-//            $ticket->setTypeTicket($data['typeTicket']);
-////           $dateVisit=$data['typeTicket'];
-//            dd($ticket);
-dd(strftime ("%A",strtotime(date_format($data['Ticket']['dateVisit'],"d-m-Y"))));
-        dd($Repository->countByDateVisit($data['dateVisit']));
+           $ticket= $form->getData();
+           $session->set('ticket',$ticket);
+           $session->set('numberVisitor',$ticket->getNumberPlace());
 
+
+            return $this->redirectToRoute('app_visitor', [
+
+            ]);
         }
 
         return $this->render('homepage.html.twig',[
@@ -44,13 +45,105 @@ dd(strftime ("%A",strtotime(date_format($data['Ticket']['dateVisit'],"d-m-Y"))))
         ]);
     }
     /**
-     * @Route("/test", name="app_test")
+     * @Route("/visiteurs", name="app_visitor")
      */
-    public function test(TicketRepository $repository)
+    public function visitor(TicketRepository $repository,SessionInterface $session,Request $request)
 
     {
-        $orders=$repository->findAll();
-        dd($orders);
+        $form=$this->createForm(VisitorFormType::class);
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $nbVisitor=$session->get('numberVisitor');
+            $nbVisitor=$nbVisitor-1;
+            $session->set('numberVisitor',$nbVisitor);
+            if($nbVisitor<0)
+            {
+                throw new Exception("Merci de ne pas renvoyer le formulaire");
+            }
+
+
+            
+            $visitor=$form->getData();
+
+            $ticket=$session->get('ticket');
+            $ticket->addVisitor($visitor);
+            $form=$this->createForm(VisitorFormType::class);
+
+            if($nbVisitor>0){
+                return $this->render('visitor.html.twig', [
+                    'visitorForm' => $form->createView()
+                ]);
+            }
+           else
+
+               return $this->redirectToRoute('app_customer', [
+
+
+
+           ]);
+
+        }
+
+        return $this->render('visitor.html.twig', [
+            'visitorForm' => $form->createView()
+
+        ]);
+    }
+    /**
+     * @Route("/test", name="app_test")
+     */
+    public function test(TicketRepository $repository,SessionInterface $session,Request $request,CalculatePriceVisitor $priceVisitor)
+
+    {
+        $ticket=$session->get('ticket');
+        $priceVisitor->visitorPrice($ticket);
+            dd($session);
+        return $this->render('test.html.twig', [
+
+
+        ]);
+    }
+    /**
+     * @Route("/Adresse", name="app_customer")
+     */
+    public function customer(TicketRepository $repository,SessionInterface $session,Request $request)
+
+    {
+        $form=$this->createForm(CustomerFormType::class);
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $customer=$form->getData();
+            $ticket=$session->get('ticket');
+            $ticket->setCustomer($customer);
+            return $this->redirectToRoute('app_payment', [
+
+
+
+            ]);
+
+        }
+
+        return $this->render('customer.html.twig', [
+            'customerForm' => $form->createView()
+
+        ]);
+    }
+    /**
+     * @Route("/payment", name="app_payment")
+     */
+    public function payment(TicketRepository $repository,SessionInterface $session,Request $request,CalculatePriceVisitor $priceVisitor)
+
+    {
+        $ticket=$session->get('ticket');
+        $priceVisitor->visitorPrice($ticket);
+
+        dd($session);
         return $this->render('test.html.twig', [
 
 
