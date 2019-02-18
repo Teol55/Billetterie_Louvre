c7eb3de2f0a84139f343be91b37216b1af39776c
C_MARTIN <m.ch55000@gmail.com>
2019-02-18 11:20:18 +0100

    add payment and mailler

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
 use App\Entity\Ticket;
+use App\Entity\Visitor;
 use App\Form\CustomerFormType;
 use App\Form\OrderFormType;
 use App\Form\VisitorFormType;
 use App\Repository\TicketRepository;
 use App\Repository\VisitorRepository;
 use App\Service\CalculatePriceVisitor;
-use mysql_xdevapi\Exception;
+use App\Service\StripeService;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -119,14 +123,80 @@ class TicketController extends AbstractController
     /**
      * @Route("/payment", name="app_payment")
      */
-    public function payment(TicketRepository $repository, SessionInterface $session, Request $request, CalculatePriceVisitor $priceVisitor)
+    public function payment( SessionInterface $session, Request $request, CalculatePriceVisitor $priceVisitor,StripeService $stripeClient,ObjectManager $em,\Swift_Mailer $mailer)
 
     {
         $ticket = $session->get('ticket');
         $priceVisitor->visitorPrice($ticket);
 
-        dd($session);
-        return $this->render('test.html.twig', [
+        if ($request->isMethod('POST')) {
+            $token = $request->request->get('stripeToken');
+
+
+
+
+            /** @var Customer $customer */
+
+            $customer = $ticket->getCustomer();
+
+            if (!$customer->getStripeCustomerId()) {
+
+
+                $stripeClient->createCustomer($customer, $token);
+            } else {
+
+                $stripeClient->updateCustomerCard($customer, $token);
+            }
+            /**@var Visitor $visitor */
+            foreach ($ticket->getVisitors() as $visitor) {
+                $em->persist($visitor);
+                $stripeClient->createInvoiceItem(
+                    $visitor->getPrice() * 100,
+                    $customer,
+                    $visitor->getName()." ".$visitor->getFirstName()
+
+                );
+            }
+            $stripeClient->createInvoice($customer, true);
+            $ticket->setCreatedAt(new \DateTime());
+            $ticket->setReference(date_format($ticket->getCreatedAt(),'Ymd').$customer->getStripeCustomerId());
+            $em->persist($ticket->getCustomer());
+
+            $em->persist($ticket);
+            $em->flush();
+            $message=(new \Swift_Message('Confirmation de Commande'))
+                ->SetFrom('Billettrie@louvre.fr')
+                ->setTo($customer->getAdresseEmail())
+                ->setBody($this->renderView('email.html.twig',
+                    ['ticket'=> $ticket]),'text/html');
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Order Complete! Yay!');
+
+            return $this->redirectToRoute('app_confirmation');
+
+        }
+
+
+
+        return $this->render('creditCard.html.twig', [
+            'ticket'=> $ticket,
+
+
+        ]);
+    }
+
+    /**
+     * @Route("/confirmation", name="app_confirmation")
+     */
+    public function confirmation( SessionInterface $session, Request $request, CalculatePriceVisitor $priceVisitor,StripeService $stripe)
+
+    {
+        $ticket = $session->get('ticket');
+
+
+        return $this->render('confirmation.html.twig', [
+            'ticket'=> $ticket,
 
 
         ]);
