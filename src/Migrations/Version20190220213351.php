15e1ad67523ca483c95e1df130e7fc1cfa16fc28
C_MARTIN <m.ch55000@gmail.com>
2019-02-22 15:30:07 +0100

    migration database

--- src/Migrations/Version20190220213351.php
+++ src/Migrations/Version20190220213351.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190208152327 extends AbstractMigration
+final class Version20190220213351 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,8 @@ final class Version20190208152327 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, adresse_email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, customer_id INT NOT NULL, date_visit DATETIME NOT NULL, type_ticket VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, price DOUBLE PRECISION NOT NULL, reference VARCHAR(255) NOT NULL, INDEX IDX_97A0ADA39395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, adresse_email VARCHAR(255) NOT NULL, stripe_customer_id VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_81398E0988D20D42 (adresse_email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE visitor (id INT AUTO_INCREMENT NOT NULL, ticket_id INT NOT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, birthday DATETIME NOT NULL, reduction TINYINT(1) DEFAULT NULL, price DOUBLE PRECISION DEFAULT NULL, INDEX IDX_CAE5E19F700047D2 (ticket_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA39395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
         $this->addSql('ALTER TABLE visitor ADD CONSTRAINT FK_CAE5E19F700047D2 FOREIGN KEY (ticket_id) REFERENCES ticket (id)');
@@ -34,10 +34,10 @@ final class Version20190208152327 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE ticket DROP FOREIGN KEY FK_97A0ADA39395C3F3');
         $this->addSql('ALTER TABLE visitor DROP FOREIGN KEY FK_CAE5E19F700047D2');
-        $this->addSql('DROP TABLE customer');
+        $this->addSql('ALTER TABLE ticket DROP FOREIGN KEY FK_97A0ADA39395C3F3');
         $this->addSql('DROP TABLE ticket');
+        $this->addSql('DROP TABLE customer');
         $this->addSql('DROP TABLE visitor');
     }
 }
